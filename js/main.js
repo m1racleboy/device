b14c1c0a091691ff7e18b0fc1dea086225f4ee38
Nikita <miracleboy3228@mail.ru>
2020-10-25 15:22:14 +0300

    fix js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-//  pop-up
-
 const contactsButton = document.querySelector('.contacts .button');
 const modalWriteUs = document.querySelector('.modal-write-us');
 const map = document.querySelector('.map');
@@ -35,97 +33,71 @@ closeMap.addEventListener('click', function (evt) {
   modalMap.classList.remove('modal-show');
 });
 
-form.addEventListener("submit", function (evt) {
-  if (!nameField.value || !nameField.validity.valid) {
-    nameField.classList.add('write-us-input-invalid');
-    evt.preventDefault();
-  } else {
-    nameField.classList.remove('write-us-input-invalid');
-  }
-  if (!emailField.value || !emailField.validity.valid) {
-    emailField.classList.add('write-us-input-invalid');
-    evt.preventDefault();
-  } else {
-    emailField.classList.remove('write-us-input-invalid');
-  }
-  if (!messageField.value) {
-    messageField.classList.add('write-us-input-invalid');
-    evt.preventDefault();
-  } else {
-    messageField.classList.remove('write-us-input-invalid');
-  }
-});
+let slideIndexTop = 1;
+showSlidesTop(slideIndexTop);
 
-// slider-top
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlide() {
-    showSlides(slideIndex += 1);
+function plusSlideTop() {
+    showSlidesTop(slideIndexTop += 1);
 }
 
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
+function minusSlideTop() {
+    showSlidesTop(slideIndexTop -= 1);  
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentSlideTop(n) {
+    showSlidesTop(slideIndexTop = n);
 }
 
-function showSlides(n) {
+function showSlidesTop(n) {
     let i;
-    let slides = document.getElementsByClassName("slider-item");
+    let slidesTop = document.getElementsByClassName("slider-item");
     let dots = document.getElementsByClassName("slider-button");
-    if (n > slides.length) {
-      slideIndex = 1;
+    if (n > slidesTop.length) {
+      slideIndexTop = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length;
+        slideIndexTop = slidesTop.length;
     }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("slide-current");
+    for (i = 0; i < slidesTop.length; i++) {
+        slidesTop[i].classList.remove("slide-current");
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].classList.remove("current-button");
     }
-    slides[slideIndex - 1].classList.add("slide-current");
-    dots[slideIndex - 1].classList.add("current-button");
+    slidesTop[slideIndexTop - 1].classList.add("slide-current");
+    dots[slideIndexTop - 1].classList.add("current-button");
 }
 
+let slideIndexBottom = 1;
+showSlideBottom(slideIndexBottom );
 
-// slider-bottom
-
-let sliderIndex = 1;
-showSlide(sliderIndex);
-
-function plusSlide() {
-    showSlide(sliderIndex += 1);
+function plusSlideBottom() {
+    showSlideBottom(slideIndexBottom  += 1);
 }
 
-function minusSlide() {
-    showSlide(sliderIndex -= 1);  
+function minusSlideBottom() {
+    showSlideBottom(slideIndexBottom  -= 1);  
 }
 
-function currentSlides(n) {
-    showSlide(sliderIndex = n);
+function currentSlidesBottom(n) {
+    showSlideBottom(slideIndexBottom  = n);
 }
 
-function showSlide(n) {
-    let slide = document.getElementsByClassName("feature-slide");
+function showSlideBottom(n) {
+    let slidesBottom = document.getElementsByClassName("feature-slide");
     let item = document.getElementsByClassName("features-item");
-    if (n > slide.length) {
-      sliderIndex = 1;
+    if (n > slidesBottom.length) {
+      slideIndexBottom  = 1;
     }
     if (n < 1) {
-        sliderIndex = slide.length;
+      slideIndexBottom  = slidesBottom.length;
     }
-    for (i = 0; i < slide.length; i++) {
-        slide[i].classList.remove("current-slide");
+    for (i = 0; i < slidesBottom.length; i++) {
+      slidesBottom[i].classList.remove("current-slide");
     }
     for (i = 0; i < item.length; i++) {
         item[i].classList.remove("current");
     }
-    slide[sliderIndex - 1].classList.add("current-slide");
-    item[sliderIndex - 1].classList.add("current");
+    slidesBottom[slideIndexBottom  - 1].classList.add("current-slide");
+    item[slideIndexBottom  - 1].classList.add("current");
 }
